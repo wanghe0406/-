82edbe345c356b58d59b979efaafe31cf5f094a2
Lough <1730790915@qq.com>
2021-09-03 16:57:50 +0800

    初次提交

---
+++ src/settings.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: lough
+ * @Date: 2021-09-03 16:31:53
+ * @LastEditors: lough
+ * @LastEditTime: 2021-09-03 16:42:13
+ * @Description: 1
+ */
+module.exports = {
+
+  title: '学生教育数据研究',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
---
+++ src/views/educationAnalyse/index.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: lough
+ * @Date: 2021-09-03 16:31:53
+ * @LastEditors: lough
+ * @LastEditTime: 2021-09-03 16:43:34
+ * @Description: 1
+-->
+<template>
+  <div>
+    1
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'EducationAnalyse',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
